1021fdd33a613b8cecc6834d623c566e351fd546
shastin245 <valexanderjustin@gmail.com>
2023-04-24 02:25:26 -0600

    final update
    Todo listo, solo falta agregar los proyectos y la hoja de vida

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
+/*==Icono de menú despegable, está función sirve para que el ícono (no el menú en general)  se retraiga o expanda al darle click==*/
+let menuIcon = document.querySelector('#icono');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
 /*==Link activo por sección, este script dejará un "active" en el menú según en la sección que se encuentre==*/
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
@@ -22,4 +31,34 @@ window.onscroll = () => {
     let header = document.querySelector('header');
 
     header.classList.toggle('sticky', window.scrollY > 100);
+
+    /*========================Función que permite que el menú despegable se retraiga de forma automática al darle click
+    y no permanezca en pantalla mientras se navega en el sitio===================================*/
+    menuIcon.classList.remove('bx-x');      /* ('bx-x') es la manera con la que referenciamos el ícono "x", proveniente de https://boxicons.com*/
+    navbar.classList.remove('active');
+
 };
+
+/*===============================================Scroll Reveal (animación de contenido en #home)=============================================================*/
+ScrollReveal({
+   // reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-cont, .hdng', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .serv-cont, .port-box, .contacto form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-cont h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-cont p, .abt-cont', { origin: 'right' });
+
+
+/*===============================================================================typed js=====================================================================*/
+
+const typed = new Typed('.txt-multi', {
+    strings : ['Desarrollador Frontend', 'Creador de contenido', 'IT Support Specialist'],
+    typeSpeed: 75,
+    backSpeed: 75,
+    backDelay: 100,
+    loop: true
+});
